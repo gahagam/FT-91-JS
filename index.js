3741b15f38f167844322c054a477ce07d43a63b0
gahagam <kharkhavkinam@gmail.com>
2025-02-23 12:03:21 +0400

    Лабораторная 3

--- index.js
+++ index.js
@@ -1,69 +1,71 @@
-function explainTask1() {
-    alert(
-        'Напишите функцию, которая принимает три числовых параметра и возвращает произведение двух максимальных из них.'
-    );
-}
 
-function explainTask2() {
-    alert(
-        'Объявите функцию, которая принимает на вход целые числа. Для чётных значений функция возвращает стрелочное выражение возведения числа в квадрат, а для нечётных – стрелочное выражение возведения в куб.'
-    );
-}
+const studentJSON = {
+    "фамилия": "Иванов",
+    "имя": "Иван",
+    "marks": [
+        { "subject": "Математика", "mark": 5 },
+        { "subject": "Физика", "mark": 4 },
+        { "subject": "Информатика", "mark": 5 },
+        { "subject": "Математика", "mark": 3 }
+    ]
+};
 
-function explainTask3() {
-    alert(
-        'Напишите рекурсивную функцию для расчёта факториала числа n.'
-    );
-}
-
-function Multi(num1, num2, num3) {
-    const max1 = Math.max(num1, num2, num3);
-    let max2;
-    if (max1 === num1) {
-        max2 = Math.max(num2, num3)
-    } else if (max1 === num2) {
-        max2 = Math.max(num1, num3)
-    } else {
-        max2 = Math.max(num1, num2)
-    }
-    return max1 * max2;
-}
-
-function Check(number) {
-    if (number % 2 === 0) {
-        return (x) => x * x;
-    } else {
-        return (x) = x * x * x;
+function printStudentInfo(student) {
+    console.log("Фамилия: " + student.фамилия);
+    console.log("Имя: " + student.имя);
+    console.log("Оценки:");
+    for (const mark of student.marks) {
+        console.log("  " + mark.subject + ": " + mark.mark);
     }
 }
 
-function factorial(n) {
-    if (n === 0) {
-        return 1;
-    }
-    else {
-        return n * factorial(n - 1);
+printStudentInfo(studentJSON);
+
+
+
+class Mark {
+    constructor(subject, mark) {
+        this.subject = subject;
+        this.mark = mark;
     }
 }
 
-function inputNumber() {
-    return parseFloat(prompt('Введите число'));
+class Student {
+    constructor(фамилия, имя, marks = []) {
+        this.фамилия = фамилия;
+        this.имя = имя;
+        this.marks = marks;
+    }
+
+
+    getAverage() { //ср оценка
+        if (this.marks.length === 0) {
+            return 0;
+        }
+        let sum = 0;
+        for (const mark of this.marks) {
+            sum += mark.mark;
+        }
+        return sum / this.marks.length;
+    }
+
+
+    getMarksBySubject(subject) { //возвр
+        const subjectMarks = [];
+        for (const mark of this.marks) {
+            if (mark.subject === subject) {
+                subjectMarks.push(mark.mark);
+            }
+        }
+        return subjectMarks;
+    }
+
+
+    addMark(subject, mark) {
+        this.marks.push(new Mark(subject, mark));
+    }
+
+    removeMarksBySubject(subject) {
+        this.marks = this.marks.filter(mark => mark.subject !== subject);
+    }
 }
-
-
-explainTask1()
-const n1 = inputNumber();
-const n2 = inputNumber();
-const n3 = inputNumber();
-alert(`Произведение двух максимальных чисел: ${Multi(n1, n2, n3)}`);
-console.log(`Произведение двух максимальных чисел: ${Multi(n1, n2, n3)}`);
-
-explainTask2()
-const n = inputNumber();
-alert(`Ваш результат: ${Check(n)}`);
-console.log(`Ваш результат: ${Check(n)}`);
-
-explainTask3()
-const number = inputNumber();
-alert(`Факториал числа: ${factorial(number)}`);
-console.log(`Факториал числа: ${factorial(number)}`);
